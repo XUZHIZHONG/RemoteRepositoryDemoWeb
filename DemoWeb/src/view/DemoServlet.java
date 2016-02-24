fbda0e5893463b4e8117f9a67d2a7e29a5cbd643
BearXu <a5873120@gmail.com>
2016-02-24 14:56:07 +0800

    加入中文處理相關功能
    修改DemoServlet

--- DemoWeb/src/view/DemoServlet.java
+++ DemoWeb/src/view/DemoServlet.java
@@ -9,29 +9,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/view/DemoServlet")
 public class DemoServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		this.doPost(request, response);
+
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		System.out.println("name=" + name);
+		String[] games = request.getParameterValues("game");
+		String size = request.getParameter("size");
+
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		String method = request.getMethod();
+		out.println("<h3>method:" + method + "</h3>");
+
+		String uri = request.getRequestURI();
+		out.println("<h3>uri:" + uri + "</h3>");
+
+		out.println("<h3>name:" + name + "</h3>");
+		out.println("<h3>size:" + size + "</h3>");
+		if (games != null && games.length != 0) {
+			for (int i = 0; i < games.length; i++) {
+				out.println("<h3>games:" + games[i] + "</h3>");
+
+			}
+
+		}
+		out.close();
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String username = request.getParameter("username");
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Demo</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h3>Hello "+username+"</h3>");
-		out.println("</body>");
-		out.println("</html>");
+		this.doGet(request, response);
 	}
 
 }
